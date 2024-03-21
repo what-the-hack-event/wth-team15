5d3fe2c85908f67eb3514c51334212c805eddf2c
Marcin Frackiewicz <44134403+MartinFrac@users.noreply.github.com>
2024-03-21 15:06:28 +0000

    Update Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
-
+//test
 namespace RazorPagesTestSample
 {
     public class Program
